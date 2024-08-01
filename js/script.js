8b32a62ef6de29bb90cd048cf743f1299546e6a4
orazi-paolo <orazi.paolo10@gmail.com>
2024-08-01 19:34:04 +0200

    added a simple validation

--- js/script.js
+++ js/script.js
@@ -21,42 +21,48 @@ button.addEventListener('click', function(){
     const fullName = fullNameField.value;
     const age = ageField.value;
 
-    // imposto il prezzo base e i sconti
-    let price = km * 0.21;
-    const discountTwenty = price * 0.8;
-    const discountForty = price * 0.6;
+    if (fullName.length > 0 || km > 0 || age > 0){
 
-    // preparo i calcoli qual ora ci sono i sconti
-    if(age < 18){
-        price = discountTwenty;
-    }else if (age > 65){
-        price = discountForty;
+        
+        // imposto il prezzo base e i sconti
+        let price = km * 0.21;
+        const discountTwenty = price * 0.8;
+        const discountForty = price * 0.6;
+        
+        // preparo i calcoli qual ora ci sono i sconti
+        if(age < 18){
+            price = discountTwenty;
+        }else if (age > 65){
+            price = discountForty;
+        }
+        // mostro il tutto in console
+        console.log('Nome e cognome ' + fullName);
+        console.log('Chilometri da percorrere ' + km);
+        console.log('Età ' + age);
+        console.log('Il prezzo del biglietto è di ' + price.toFixed(2) + ' euro');
+        
+        // inserisco nel biglietto i risultati
+        document.getElementById('fullNameResult').innerText = fullName;
+        document.getElementById('priceResult').innerText = price.toFixed(2) + '$'
+        
+        // seleziono il div per il numero casuale della carrozza e lo assegno
+        let casualNumber = document.getElementById('casualNumber');
+        let numberOneToNine = numberCasualGeneration(1,9);
+        casualNumber.innerText = numberOneToNine;
+        console.log( 'il numero casuale della carrozza è ' + numberOneToNine);
+        
+        // seleziono il div per il numero casuale del biglietto e lo assegno
+        let numberTicket = document.getElementById('codeCP');
+        let numberCasualTicket = numberCasualGeneration(90000,99999);
+        numberTicket.innerText = numberCasualTicket;
+        console.log('il numero casuale del biglietto è ' + numberCasualTicket);
+        
+        // resetto il form al click
+        let form = document.getElementById('form');
+        form.reset();
+    }else{
+        alert('i valori non sono esatti');
     }
-    // mostro il tutto in console
-    console.log('Nome e cognome ' + fullName);
-    console.log('Chilometri da percorrere ' + km);
-    console.log('Età ' + age);
-    console.log('Il prezzo del biglietto è di ' + price.toFixed(2) + ' euro');
-
-    // inserisco nel biglietto i risultati
-    document.getElementById('fullNameResult').innerText = fullName;
-    document.getElementById('priceResult').innerText = price.toFixed(2) + '$'
-
-    // seleziono il div per il numero casuale della carrozza e lo assegno
-    let casualNumber = document.getElementById('casualNumber');
-    let numberOneToNine = numberCasualGeneration(1,9);
-    casualNumber.innerText = numberOneToNine;
-    console.log( 'il numero casuale della carrozza è ' + numberOneToNine);
-
-    // seleziono il div per il numero casuale del biglietto e lo assegno
-    let numberTicket = document.getElementById('codeCP');
-    let numberCasualTicket = numberCasualGeneration(90000,99999);
-    numberTicket.innerText = numberCasualTicket;
-    console.log('il numero casuale del biglietto è ' + numberCasualTicket);
-
-    // resetto il form al click
-    let form = document.getElementById('form');
-    form.reset();
 
 })
 
